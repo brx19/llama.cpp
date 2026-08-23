1bcc56925d5ac750e3285b242a8346e8aac2ae08
leonardHONG <2695316095@qq.com>
2026-08-23 13:34:00 +0800

    cuda: limit cutlass repack path to dense models

--- ggml/src/ggml-cuda/mmvq.cu
+++ ggml/src/ggml-cuda/mmvq.cu
@@ -836,10 +836,6 @@ static __global__ void mul_mat_vec_nvfp4_repacked_f32(
                 case GGML_GLU_OP_GEGLU:
                     sum *= ggml_cuda_op_gelu_single(gate_sum);
                     break;
-                case GGML_GLU_OP_SWIGLU_OAI:
-                    sum = ggml_cuda_op_swiglu_oai_single(
-                        gate_sum, sum, fusion.swiglu_oai_alpha, fusion.swiglu_oai_limit);
-                    break;
                 default:
                     sum *= gate_sum;
                     break;
@@ -1202,8 +1198,7 @@ static __global__ void mul_mat_vec_q(
                                 result *= ggml_cuda_op_gelu_single(gate_value);
                                 break;
                             case GGML_GLU_OP_SWIGLU_OAI:
-                                result = ggml_cuda_op_swiglu_oai_single(
-                                    gate_value, result, fusion.swiglu_oai_alpha, fusion.swiglu_oai_limit);
+                                result = ggml_cuda_op_swiglu_oai_single(gate_value, result);
                                 break;
                             case GGML_GLU_OP_SWIGLU_CLAMP:
                                 result = ggml_cuda_op_swiglu_clamp_single(gate_value, result, glu_limit);
@@ -1919,8 +1914,6 @@ void ggml_cuda_mul_mat_vec_q(
             fusion_local.gate_scale = fusion->gate_scale->data;
         }
         fusion_local.glu_op = fusion->glu_op;
-        fusion_local.swiglu_oai_alpha = fusion->swiglu_oai_alpha;
-        fusion_local.swiglu_oai_limit = fusion->swiglu_oai_limit;
     }
 
 #ifdef GGML_CUDA_CUTLASS
